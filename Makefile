a54988a7e3f6f90795ed0d8d1b4e369c922bd460
Lourens KOEKEMOER <lkoekemo@e4r2p3.wethinkcode.co.za>
2017-09-29 00:00:45 +0200

    0

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-asmtut: asmtut.o
-		ld -o asmtut asmtut.o
-asmtut.o: asmtut.s
-		as -o asmtut.o asmtut.s
-clean:
-		rm *.o asmtut
+all:
+	nasm -f macho64 hello3.s -o hello3.o
+	ld hello3.o -macosx_version_min 10.8 -lSystem
+
+linux:
+	nasm -f elf64 hello3.s -o hello3.o
+	ld hello3.o
--- hello1.s
+++ hello1.s
@@ -4,13 +4,13 @@
 
 section .data
 hello:
-	.string db "Hello World!", 12
+	.string db "Hello World!", 10
 	.len equ $ - hello.string
 
 section .text
 	global start
 	global _main
-	exterm _printf
+	;extern _printf
 
 start:
 	call _main
@@ -21,7 +21,7 @@ _main:
 	mov rbp, rsp
 	sub rsp, 16
 	mov rdi, 1
-	lea rdi, [rel hello.string]
+	lea rsi, [rel hello.string]
 	mov rdx, hello.len
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
